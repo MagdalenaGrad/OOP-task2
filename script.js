862f91f58eed8aefe9700e92aeeb86a0f4c909d0
Magdalena Grad <mgdln.grad@gmail.com>
2018-07-28 19:14:29 +0200

    add task2

---
+++ script.js
@@ -0,0 +1,26 @@
+'use strict'
+
+function Button(text){
+	this.text = text || 'Hello';
+}
+
+Button.prototype = {
+	create: function(){
+		var self = this;
+		this.element = document.createElement('button');
+		this.element.innerText = this.text;
+		this.element.addEventListener('click', function(){
+			alert(self.text);
+		})
+		document.body.appendChild(this.element);
+	}
+}
+
+var btn1 = new Button('Hello!');
+btn1.create();
+
+var btn2 = new Button('Welcome!');
+btn2.create();
+
+var btn3 = new Button('Good morning!');
+btn3.create();
